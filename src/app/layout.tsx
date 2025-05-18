e0efc5288ffa92ff0ad2c18e7ab3b7d3f4d3bf85
rasoolgh71 <rasoolghanna@gmail.com>
2025-05-18 14:16:18 +0330

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <h2>hello rasool</h2>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ src/pages/about.tsx
@@ -0,0 +1,5 @@
+// import Image from "next/image";
+
+export default function About() {
+  return <h2> about me</h2>
+}
